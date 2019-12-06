e73986f011d8dbaf87e567831cf6406a207cd290
Saikat Bhadury <info2programmer@gmail.com>
2019-12-06 19:42:03 +0530

    Home Page Langualge Impliment

--- www/js/app.js
+++ www/js/app.js
@@ -2,71 +2,77 @@
 var $$ = Dom7;
 
 // Framework7 App main instance
-var app  = new Framework7({
-  root: '#app', // App root element
-  id: 'io.framework7.testapp', // App bundle ID
-  name: 'Framework7', // App name
-  theme: 'auto', // Automatic theme detection
+var app = new Framework7({
+  root: "#app", // App root element
+  id: "io.framework7.testapp", // App bundle ID
+  name: "Framework7", // App name
+  theme: "auto", // Automatic theme detection
   // App root data
-  data: function () {
+  data: function() {
     return {
       user: {
-        firstName: 'John',
-        lastName: 'Doe',
-      },
+        firstName: "John",
+        lastName: "Doe"
+      }
     };
   },
   // App root methods
   methods: {
-    helloWorld: function () {
-      app.dialog.alert('Hello World!');
-    },
+    helloWorld: function() {
+      app.dialog.alert("Hello World!");
+    }
   },
   // App routes
   routes: routes,
   // Enable panel left visibility breakpoint
   panel: {
     swipe: true,
-    leftBreakpoint: 960,
-  },
+    leftBreakpoint: 960
+  }
 });
 
 // Init/Create left panel view
-var leftView = app.views.create('.view-left', {
-  url: '/'
+var leftView = app.views.create(".view-left", {
+  url: "/"
 });
 
 // Init/Create main view
-var mainView = app.views.create('.view-main', {
-  url: '/'
+var mainView = app.views.create(".view-main", {
+  url: "/"
 });
 
 // Login Screen Demo
-$$('#my-login-screen .login-button').on('click', function () {
+$$("#my-login-screen .login-button").on("click", function() {
   var username = $$('#my-login-screen [name="username"]').val();
   var password = $$('#my-login-screen [name="password"]').val();
 
   // Close login screen
-  app.loginScreen.close('#my-login-screen');
+  app.loginScreen.close("#my-login-screen");
 
   // Alert username and password
-  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
+  app.dialog.alert("Username: " + username + "<br>Password: " + password);
 });
 
 var myPhotoBrowserPopupDark = app.photoBrowser.create({
-  photos : [
-      {
-          html: '<iframe src="//www.youtube.com/embed/lmc21V-zBq0" frameborder="0" allowfullscreen></iframe>',
-          caption: 'Woodkid - Run Boy Run (Official HD Video)'
-      },
-      {
-        html: '<iframe src="//www.youtube.com/embed/lmc21V-zBq0" frameborder="0" allowfullscreen></iframe>',
-          caption: 'Second Caption Text'
-      },
-      {
-        html: '<iframe src="//www.youtube.com/embed/lmc21V-zBq0" frameborder="0" allowfullscreen></iframe>',
-      },
+  photos: [
+    {
+      html:
+        '<iframe src="//www.youtube.com/embed/lmc21V-zBq0" frameborder="0" allowfullscreen></iframe>',
+      caption: "Woodkid - Run Boy Run (Official HD Video)"
+    },
+    {
+      html:
+        '<iframe src="//www.youtube.com/embed/lmc21V-zBq0" frameborder="0" allowfullscreen></iframe>',
+      caption: "Second Caption Text"
+    },
+    {
+      html:
+        '<iframe src="//www.youtube.com/embed/lmc21V-zBq0" frameborder="0" allowfullscreen></iframe>'
+    }
   ],
-  theme: 'dark',
-  type: 'standalone'
+  theme: "dark",
+  type: "standalone"
 });
+
+// App Loading Code
+phonegapApp.appLoading();
--- www/js/index.js
+++ www/js/index.js
@@ -1,22 +1,64 @@
 var phonegapApp = {
-    // Application Constructor
-    initialize: function() {
-       // this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-       // document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-       // app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    
+  // Application Constructor
+  initialize: function() {
+    // this.bindEvents();
+  },
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+    // document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicitly call 'app.receivedEvent(...);'
+  onDeviceReady: function() {
+    phonegapApp.appLoading();
+  },
+  // Update DOM on a Received Event
+
+  //  This Function For App Launch Component
+  appLoading: function() {
+    let language = localStorage.getItem("localLanguage");
+
+    if (language == "") {
+      localStorage.setItem("localLanguage", "en");
+    }
+
+    switch (language) {
+      case "en":
+        $("#homePageTitle").html(`Home`);
+        $("#lblTemplaeSchedule").html(`Temple schedule`);
+        $("#lblDailyProsad").html(`Daily Prosad`);
+        $("#lblSpecialProsad").html(`Special Prosad`);
+        $("#lblTodayEvent").html(`Today Event`);
+        $("#lblVaisavCalender").html(`Vaisav Calender`);
+        $("#lblNityaSeba").html(`Nitya Seba`);
+        break;
+
+      case "bn":
+        $("#homePageTitle").html(`হোম`);
+        $("#lblTemplaeSchedule").html(`মন্দিরের সময়সূচী`);
+        $("#lblDailyProsad").html(`দৈনিক প্রসাদ`);
+        $("#lblSpecialProsad").html(`বিশেষ প্রসাদ`);
+        $("#lblTodayEvent").html(`আজ ইভেন্ট`);
+        $("#lblVaisavCalender").html(`বৈষ্ণৱ ক্যালেন্ডার`);
+        $("#lblNityaSeba").html(`নিত্য সেবা`);
+        break;
+
+      case "hin":
+        $("#homePageTitle").html(`होम`);
+        $("#lblTemplaeSchedule").html(`मंदिर का कार्यक्रम`);
+        $("#lblDailyProsad").html(`दैनिक पेशेवरों`);
+        $("#lblSpecialProsad").html(`बिसेष अभियोजन`);
+        $("#lblTodayEvent").html(`आज घटना`);
+        $("#lblVaisavCalender").html(`वैष्णव कैलेंडर`);
+        $("#lblNityaSeba").html(`नित्य सेबा`);
+        break;
+      default:
+        break;
+    }
+  }
 };
